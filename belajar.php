fa7084b578f5f9ca5d45767435344025f6f77aba
susan safira <safirasusan16@gmail.com>
2021-03-02 16:37:16 +0700

    belajar php dasar

---
+++ belajar.php
@@ -0,0 +1,20 @@
+<?php
+
+    $angka = 1;
+    echo $angka;
+
+    $angka_desimal = 3.5;
+    echo "</br>";
+    echo $angka_desimal;
+
+    echo "</br>";
+    echo "dokumen saya : ".$_SERVER["DOCUMENT_ROOT"];
+
+    $nama = susan safira
+    echo "<br/>
+    echo  $nama";
+
+    define("PHI",3.14);
+    $jari_jari = 7
+    $
+?>
